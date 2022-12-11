a8a387caf4470c4203ee8b904127f43e11e55ae5
Petter Rasmussen <petter@hask.no>
2022-12-11 14:40:52 +0200

    Disable debug logging

--- seedphrase_web/src/home_page.ts
+++ seedphrase_web/src/home_page.ts
@@ -1,16 +1,11 @@
 import init from "../wasm/seedphrase.js";
 import { homePage } from "../wasm/seedphrase";
 import { Poly } from "poly";
-import { defaultDebugConfig } from "poly/src/logger";
 
 (async () => {
   await init("/wasm/seedphrase_bg.wasm");
 
   const page = homePage(location.href);
-
-  const poly = new Poly(page, {
-    loggerConfig: defaultDebugConfig(),
-  });
-
+  const poly = new Poly(page);
   poly.init();
 })();
