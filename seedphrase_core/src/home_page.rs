8c67262fe8584094e2b3fc0c38ff553ee0e99289
Petter Rasmussen <petter@hask.no>
2022-12-12 10:10:03 +0200

    Add footer

--- seedphrase_core/src/home_page.rs
+++ seedphrase_core/src/home_page.rs
@@ -133,7 +133,7 @@ fn view_head() -> maud::Markup {
 fn view_body(model: &Model) -> maud::Markup {
     html! {
         div id=(Id::Seedphrase) class="sm:pt-8 mx-auto max-w-7xl sm:px-6 lg:px-8" {
-            div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow" {
+            main class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow" {
                 div class="px-4 py-5 sm:px-6" {
                     (view_input(model))
                     p class="mt-2" {
@@ -158,8 +158,30 @@ fn view_body(model: &Model) -> maud::Markup {
                     }
                 }
             }
-        }
 
+
+            footer {
+                div class="mt-8 flex justify-between border-t border-gray-200 text-center text-xs text-gray-500 sm:text-left" {
+                    span class="flex flex-col justify-end mt-4" {
+                        "A glotlabs project."
+                    }
+
+                    span class="flex flex-col justify-end mt-4" {
+                        a href="https://github.com/glotlabs/seedphrase" target="_blank" {
+                            (github_icon())
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn github_icon() -> Markup {
+    html! {
+        svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" {
+            path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" {}
+        }
     }
 }
 
